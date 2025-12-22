a882125ded00f8a46cb385383f346d3f5b367c12
otaviouu <otavio.venancio.oliveira@usp.br>
2025-12-22 19:21:45 -0300

    auth generator init

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :so2_float, :scopes,
+  user: [
+    default: true,
+    module: So2Float.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: So2Float.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :so2_float,
   ecto_repos: [So2Float.Repo],
   generators: [timestamp_type: :utc_datetime]
